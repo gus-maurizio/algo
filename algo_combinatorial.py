0f686af236ac9d6e267a81bf43530eb510dc88a2
gustavo maurizio <gustavo.maurizio@capitalone.com>
2022-05-04 08:49:05 -0400

    Wed May  4 08:49:05 EDT 2022

--- algo_combinatorial.py
+++ algo_combinatorial.py
@@ -1,5 +1,13 @@
 import random,sys
 
+def count1bits(n):
+    c = 0
+    i = n
+    while i > 0:
+        c += 1
+        i &= (i-1)
+    return c
+
 def p(n=4,i=0,a=[]):
     if i < n:
         for j in range(n):
@@ -22,26 +30,16 @@ def c(n=4,m=2,i=0,a=[]):
             yield a
 
 def subsets(n=4):
-    masks = [1 << i for i in range(n)]
-    alle  = list(range(n))
     for i in range(1 << n):             # loop 2**n times with bit shift from 0 to 2**n. n bits from all 0 to all 1 (2**n-1)
-        yield [ss for mask, ss in zip(masks, alle) if i & mask]
+        yield [ss for mask, ss in zip([1 << i for i in range(n)], range(n)) if i & mask]
 
-def subsetsm(n=4,m=2):
-    # return only subsets of size m (Exactly)
-    masks = [1 << i for i in range(n)]
-    alle  = list(range(n))
-    for i in range(1 << n):             # loop 2**n times with bit shift from 0 to 2**n. n bits from all 0 to all 1 (2**n-1)
-        a = [ss for mask, ss in zip(masks, alle) if i & mask]
-        if len(a) == m:
-            yield a
+def subsetsm(n=4,m=2):                  # return only subsets of size m (Exactly)
+    for i in [x for x in range(1 << n) if count1bits(x) == m]:
+        yield [ss for mask, ss in zip([1 << i for i in range(n)], range(n)) if i & mask]
 
 def powerset(n=4):
     if n == 0:
         yield set()
-    elif n == 1:
-        yield set()
-        yield {n}
     else:
         for item in powerset(n-1):
             yield {n} | item
