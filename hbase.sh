b1bba983e9bb5b6fa5fe1d215b4c0614cbba9cab
gustavo maurizio <gustavo.maurizio@capitalone.com>
2022-06-04 22:38:14 -0400

    Sat Jun  4 22:38:14 EDT 2022

---
+++ hbase.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+## Name: SSM Agent Installer Script
+## Description: Installs SSM Agent on EMR cluster EC2 instances and update hosts file
+##
+sudo yum install -y amazon-ssm-agent
+sudo service amazon-ssm-agent status >>/tmp/ssm-status.log
+## Update hosts file
+echo "########### localhost mapping check ###########" >> /tmp/localhost.log
+v_ipaddr=`hostname --ip-address`
+echo "${v_ipaddr} localhost" >>/etc/hosts
+sudo cat /etc/hosts >> /tmp/localhost.log
+
+echo "########### Exit script ########### " >> /tmp/localhost.log
