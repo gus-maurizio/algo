120116c20404c84ec129c9b0ce431760bae365c2
gustavo maurizio <gustavo.maurizio@capitalone.com>
2022-06-05 05:32:42 -0400

    Sun Jun  5 05:32:42 EDT 2022

--- hbase.sh
+++ hbase.sh
@@ -6,8 +6,8 @@ sudo yum install -y amazon-ssm-agent
 sudo service amazon-ssm-agent status >>/tmp/ssm-status.log
 ## Update hosts file
 echo "########### localhost mapping check ###########" >> /tmp/localhost.log
-v_ipaddr=`hostname --ip-address`
-echo "${v_ipaddr} localhost" >>/etc/hosts
+v_ipaddr=$(hostname --ip-address)
+echo "${v_ipaddr} localhost" >/tmp/etc.hosts
+sudo /bin/bash -c 'cat /tmp/etc.hosts >>/etc/hosts'
 sudo cat /etc/hosts >> /tmp/localhost.log
-
-echo "########### Exit script ########### " >> /tmp/localhost.log
+echo "########### Exit script ###########" >> /tmp/localhost.log
